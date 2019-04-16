f87a591c7e872f43369ea9a5e69a0c4ed8a3b9c8
Broutous <gaetanbenaloul@gmail.com>
2019-04-16 16:51:15 +0200

    Script fruits.php

---
+++ fruits.php
@@ -0,0 +1,53 @@
+<?php
+
+    // on récupère le nom du fruit passé dans l'url (?fruit=xxxx)
+    // via la superglobale $_GET.
+    // comme $_GET est un array associatif, on récupère la valeur en précisant la clé dans les crochets
+    // Attention : la clé est une chaîne ('entre quotes')
+    $fruit = $_GET['fruit'];
+
+    // on créé le tableau des fruits
+    $fruitsExistants = [
+        'banane',
+        'pomme',
+        'poire',
+        'fraise',
+        'pêche'
+    ];
+
+    // on créé le tableau associatif qui donne la couleur en hexadécimal pour chaque fruit.
+    $couleursFruits = [
+        'banane' => '#e7ea3f',
+        'pomme' => '#c4e86a',
+        'poire' => '#e7ea3f',
+        'fraise' => '#d63931',
+        'pêche' => '#f9a227'
+    ];
+
+    // on vérifie que la couleur pour le fruit donné en paramètre d'url existe dans notre tableau
+    if (!empty($couleursFruits[$fruit])) {
+        $couleurFruit = $couleursFruits[$fruit]; // si c'est le cas on l'affecte à $couleurFruit
+    } else {
+        $couleurFruit = '#ffffff'; // sinon on met du blanc par défaut.
+    }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset='utf-8'>
+   <!-- Un super titre pour être original -->
+   <title>Fruits.com, Mangez des fruits !</title>
+   
+   <!-- Un peu de style pour être poli -->
+   <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <!-- On ajoute la couleur dans l'attribut style de la balise <h1> et on ajoute le "s" pour mettre le fruit au pluriel -->
+    <h1 style="color: <?php echo $couleursFruits[$fruit] ?>">Mangez des <?php echo $fruit . 's'; ?> !</h1>
+
+    <!-- On va chercher l'image qui porte le nom du fruit passé en paramètre d'url en préfixant avec le répertoire /img et 
+         en suffixant avec l'extension .jpg -->
+    <img src="img/<?php echo $fruit; ?>.jpg" alt="<?php echo $fruit; ?>" />
+</body>
+</html>
